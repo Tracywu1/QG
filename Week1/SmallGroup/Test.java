323b7ec524f0054a53802433994ecb62ae2492d8
Tracy <3211917295@qq.com>
2023-03-24 17:31:15 +0800

    SmallGroup

---
+++ Week1/SmallGroup/Test.java
@@ -0,0 +1,64 @@
+package com.jdbc;
+
+import com.pojo.Account;
+import org.junit.Test;
+
+import java.util.List;
+
+public class demo1 {
+   @Test
+    public void testQuery(){
+        try {
+            String sql1="select id,username,password from account where id =?";
+            Account acc1 = CRUDUtils.query(sql1, Account.class,1);
+            System.out.println(acc1);
+
+            String sql2="select id,username,password from account where id =?";
+            Account acc2 = CRUDUtils.query(sql2, Account.class,2);
+            System.out.println(acc2);
+
+            String sql3="select id,username,password from account where id <=?";
+            List<Account> accs = CRUDUtils.queryMore(sql3, Account.class,2);
+            System.out.println(accs);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testAdd(){
+       try {
+           Object[] params = {"wangwu","7891"};
+           String sql = "insert into account(username,password) values(?,?)";
+           int update = CRUDUtils.update(sql, params);
+           System.out.println(update);
+       }catch (Exception e){
+           e.printStackTrace();
+       }
+   }
+   @Test
+   public void testDelete(){
+       try {
+           String sql = "delete from account where id = ?";
+           int update = CRUDUtils.update(sql, 3);
+           System.out.println(update);
+       }catch (Exception e){
+           e.printStackTrace();
+       }
+   }
+   @Test
+   public void testModify(){
+       try {
+           Object [] params = {"VIP","88888888","2"};
+           String sql = "update account set username = ?,password = ? where id = ?";
+           int update = CRUDUtils.update(sql, params);
+           System.out.println(update);
+       }catch (Exception e){
+           e.printStackTrace();
+       }
+   }
+}
+
+
+
